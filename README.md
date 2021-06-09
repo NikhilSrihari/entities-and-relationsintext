2e3e2b102f1eec47656e55b76aa4a0ad68c6e538
Nikhil Srihari <nsrihari@nvidia.com>
2021-06-08 23:26:29 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# entities-and-relationsintext
-Identifying Entities And Their Relations In Text
+# Identifying Entities And Their Relations In Text
+
+## Overview
+Identifying entities and their relations in text is useful for multiple NLP tasks, for example creating Knowledge Graphs, Text Summarization, Question Answering, etc. 
+Please refer to this doc  
+
+## Inference:
+Here are the steps to run inference:
+1. Clone this model repo:  
+``git clone -b mediumblog1 ``
+2. Starting the Docker container:  
+``docker run --gpus all -it --rm -v /mnt/dldata/nsrihari/GauGan/mediumblog1/gaugan:/gaugan --shm-size=8g -p 8888:8888 -p 6006:6006 --ulimit memlock=-1 --ulimit stack=67108864 --device=/dev/snd nvcr.io/nvidia/nemo:1.0.0rc1``
+3. Installing dependencies:  
+``pip install jsonlines``
+4. Running Inference:  
+``cd /gaugan && python inferencepipeline_interactive.py ``
+
+## Training:
+There are 2 components, both of which are trained independently:
+###1 Entities Extraction:
+Here are the steps to run training for the Entities Extraction component:
+1. Clone this model repo:  
+``git clone -b mediumblog1 ``
+
+###2 Relation Extraction:
+Here are the steps to run training for the Relation Extraction component:
+1. Clone this model repo:  
+``git clone -b mediumblog1 ``
+2. Starting the Docker container: 
+``docker run --gpus all -it --rm -v /mnt/dldata/nsrihari/GauGan/mediumblog1/gaugan:/gaugan --shm-size=8g -p 8888:8888 -p 6006:6006 --ulimit memlock=-1 --ulimit stack=67108864 --device=/dev/snd nvcr.io/nvidia/nemo:1.0.0rc1``
+3. Installing dependencies:  
+``pip install jsonlines``
+4. Preprocess data and create data that NeMo needs. 
+Make sure that the ``captions_sng.jsonl`` file exists in ``\NeMo\data`` folders. Then run: <br>  
+``cd /NeMo/data && python preprocessing.py``  <br>
+This will create 4 files: text_train.txt, text_dev.txt, labels_train.txt and labels_dev.txt 
+5. Run training
+``cd /gaugan && python relation_extraction/token_classification.py ``
---
+++ inferencepipeline_interactive.py
@@ -0,0 +1,53 @@
+import os, sys
+from entities_extraction import token_classification_inference as entities_extraction_inference
+from relation_extraction import token_classification_inference as relation_extraction_inference
+
+ENTITIES_EXTRACTION_NEMOMODEL_LOCATION = "/gaugan/entities_extraction/model/token_classification_model.nemo"
+RELATION_EXTRACTION_NEMOMODEL_LOCATION = "/gaugan/relation_extraction/model/token_classification_model.nemo"
+
+entities_extraction_model, relation_extraction_model = None, None
+
+
+def load_model():
+    global entities_extraction_model, relation_extraction_model, entities_extraction_model, relation_extraction_nemomodel_location
+    entities_extraction_model = entities_extraction_inference.load_model(ENTITIES_EXTRACTION_NEMOMODEL_LOCATION)
+    relation_extraction_model = relation_extraction_inference.load_model(RELATION_EXTRACTION_NEMOMODEL_LOCATION)
+    return entities_extraction_model, relation_extraction_model
+
+
+def inference_pipeline(input_text):
+    global entities_extraction_model, relation_extraction_model
+    entities_extraction_inference_queries = [input_text]
+    entities_extraction_inference_results = entities_extraction_inference.run_inference(entities_extraction_model, entities_extraction_inference_queries)
+    entities_pairs, relation_extraction_inference_results = relation_extraction_inference.run_inference(relation_extraction_model, input_text, entities_extraction_inference_results)
+    return entities_extraction_inference_results, entities_pairs, relation_extraction_inference_results
+    
+    
+def print_results(input_text, entities_extraction_inference_results, entities_pairs, relation_extraction_inference_results):
+    print("INPUT TEXT: ", input_text)
+    if entities_extraction_inference_results and len(entities_extraction_inference_results)!=0:
+        print("ENTITIES: ")
+        for entity in entities_extraction_inference_results[0]:
+            print("    ", entity)
+        print("RELATIONS: ")
+        for entities_pair, relation_extraction_inference_result in zip(entities_pairs, relation_extraction_inference_results):
+            print("    ", entities_pair[0], " AND ", entities_pair[1], " -> ", relation_extraction_inference_result)
+
+
+def inference_interactive_loop():
+    while True:
+        os.system('clear') 
+        input_text = input("Enter Input Text: ")
+        entities_extraction_inference_results, entities_pairs, relation_extraction_inference_results = inference_pipeline(input_text)
+        os.system('clear') 
+        print_results(input_text, entities_extraction_inference_results, entities_pairs, relation_extraction_inference_results)
+        input("\nPress Enter to continue...")
+    
+
+def main():
+    load_model()
+    inference_interactive_loop()
+    
+
+if __name__ == '__main__':
+    main()
